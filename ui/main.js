2d980a94cc9506915202cab11039b97347bb4272
palakbpatel <30790598+palakbpatel@users.noreply.github.com>
2017-08-08 16:25:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
@@ -8,4 +8,22 @@ function moveRight(){
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
     //img.style.marginLeft='100px';
-}
+}*/
+
+var button=document.getElementById('counter');
+var counter=0;
+button.onClick=function(){
+    
+    
+    //Makea request to counter endpoint
+    
+    
+    //Capture a resonse in varibale and store it
+    
+    
+    
+    //Render the variable in correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
