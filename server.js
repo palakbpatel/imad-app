7efd02e4a3a2d74a9396fdb75701e1b8f205b43c
palakbpatel <30790598+palakbpatel@users.noreply.github.com>
2017-08-08 16:18:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,14 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
+
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
  res.send(createtemplate(articles[articleName]));
